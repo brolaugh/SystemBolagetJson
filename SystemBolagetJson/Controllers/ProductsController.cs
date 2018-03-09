e3cac22600e0bcf057394aa2416a11c1fea020ca
Hannes Kindströmmer <me@brolaugh.com>
2018-03-09 22:40:18 +0100

    Seperate responsablities

---
+++ SystemBolagetJson/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using SystemBolagetJson.Models;
+using SystemBolagetJson.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SystemBolagetJson.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        public ProductsController()
+        {
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<artiklarArtikel>> Get() => await ProductRepository.GetCollectionAsync();
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var collection = await ProductRepository.GetCollectionAsync();
+            var targetProduct = collection.SingleOrDefault(p => p.nr == id);
+
+            if (targetProduct == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(targetProduct);
+        }
+    }
+}
---
+++ SystemBolagetJson/Repositories/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using SystemBolagetJson.Models;
+
+namespace SystemBolagetJson.Repositories
+{
+    public static class ProductRepository//: IRepository<artiklarArtikel>
+    {
+        private static DateTime? lastFetch;
+        private static List<artiklarArtikel> productList;
+
+        public static async Task<ICollection<artiklarArtikel>> GetCollectionAsync()
+        {
+            await Update();
+            return productList;
+        }
+
+        private static async Task UpdateProductList()
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.systembolaget.se/api/")
+            };
+            var stream = await client.GetStreamAsync("assortment/products/xml");
+
+            var serializer = new XmlSerializer(typeof(artiklar));
+            var streamReader = new StreamReader(stream);
+            var articles = (artiklar)serializer.Deserialize(streamReader);
+            streamReader.Close();
+            productList = articles.artikel.ToList();
+            lastFetch = DateTime.UtcNow;
+        }
+
+        public static async Task Update()
+        {
+            if (!lastFetch.HasValue)
+            {
+                await UpdateProductList();
+                return;
+            }
+            var timeSinceLastFetch = lastFetch.Value - DateTime.UtcNow;
+            if (timeSinceLastFetch.TotalSeconds < 60 * 60)
+            {
+                return;
+            }
+
+            await UpdateProductList();
+        }
+
+
+    }
+}
